103f757f6b7467befda91360a0a3d674b07feabb
Mr. Tee <tullah.nazari@gmail.com>
2019-07-23 12:08:10 -0500

    Skillselectionlayout

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,24 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+    }
+
+    fun gotowelcomepage(view: View) {
+        val welcomeActivity = Intent(this, WelcomeActivity::class.java)
+        startActivity(welcomeActivity)
     }
 }
